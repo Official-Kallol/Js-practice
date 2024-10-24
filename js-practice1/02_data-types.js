1e26b6cf7f9ffa306253dfb84ac46f77df8c820b
Kallol Nath <official.kallol2024@gmail.com>
2024-10-24 06:53:21 +0000

    learn datatypes

---
+++ js-practice1/02_data-types.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello world") // run only in browser 
+
+let name = "Kallol";
+let age = 22;
+let isReady = false;
+// null
+//undefined
+// symbol
+// bigint
+
+console.log(typeof name); 
+console.log(typeof age);
+console.log(typeof isReady);
+console.log(typeof null);
+console.log(typeof undefined);
