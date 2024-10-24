4629dfb58312e9120f172ecfe23840658797de3a
Kallol Nath <official.kallol2024@gmail.com>
2024-10-24 06:52:51 +0000

    variable practice

---
+++ js-practice1/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 123456
+let accountEmail = "kallolnath@gmail.com" ;
+var accountPsw = "87654321"
+accountCity = "Kolkata"
+let accountState;
+
+// accountId = 546543 // const can't be changed
+
+accountEmail = "abc@gmail.com"
+accountPsw = "741852"
+accountCity = "Bongaon"
+
+/*
+never use var
+let mean "Variable"
+*/
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPsw, accountCity,accountState]);
